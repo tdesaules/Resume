dd8b70e6f5259f9736607502d0bd5aecb99fa66e
tdesaules <tdesaules@outlook.com>
2020-11-05 15:03:43 +0000

    correct JS-0195

--- jest.config.js
+++ jest.config.js
@@ -3,21 +3,21 @@ module.exports = {
   globals: {
     'vue-jest': {}
   },
-  moduleFileExtensions: [ "js", "json", "vue" ],
+  moduleFileExtensions: [ 'js', 'json', 'vue' ],
   moduleNameMapper: {
-    "^Vue/(.*)$": "<rootDir>/src/vue/$1",
-    "^Config/(.*)$": "<rootDir>/src/config/$1",
-    "^Img/(.*)$": "<rootDir>/src/img/$1"
+    '^Vue/(.*)$': '<rootDir>/src/vue/$1',
+    '^Config/(.*)$': '<rootDir>/src/config/$1',
+    '^Img/(.*)$': '<rootDir>/src/img/$1'
   },
   transform: {
-    ".*\\.(vue)$": "vue-jest",
-    ".*\\.(js)$": "babel-jest",
-    ".*\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
+    '.*\\.(vue)$': 'vue-jest',
+    '.*\\.(js)$': 'babel-jest',
+    '.*\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
   },
   transformIgnorePatterns: [
-    "/node_modules/"
+    '/node_modules/'
   ],
   collectCoverage: true,
-  collectCoverageFrom: [ "src/**/*.{vue,js}", "!**/node_modules/**" ],
-  coverageReporters: [ "cobertura", "lcov" ]
+  collectCoverageFrom: [ 'src/**/*.{vue,js}', '!**/node_modules/**' ],
+  coverageReporters: [ 'cobertura', 'lcov' ]
 }
